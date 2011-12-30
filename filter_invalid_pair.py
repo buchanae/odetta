23871d439704434a19525411bf07e3b198608f2f
Alex Buchanan <buchanae@gmail.com>
2011-12-30 14:29:41 -0800

    remove duplicates_reducer.  code docs too.

--- filter_invalid_pair.py
+++ filter_invalid_pair.py
@@ -9,15 +9,17 @@ from alignment import ID_base, distance_between
 class FilterInvalidPair(MRJob):
 
     """
-    TODO
+    Filter paired alignments on a set of criteria including distance, strand, etc.
+
+    --min-distance and --max-distance command-line options configure allowed distance.
+    By default, any distance is allowed.
     """
 
     INPUT_PROTOCOL = JSONProtocol
 
     def configure_options(self):
-        """
-        TODO
-        """
+        """Define command-line options."""
+
         super(FilterInvalidPair, self).configure_options()
         self.add_passthrough_option('--min-distance', action='store',
                                     type=float, default=float('-inf'))
@@ -25,44 +27,42 @@ class FilterInvalidPair(MRJob):
                                     type=float, default=float('inf'))
 
     def mapper(self, ID, alignment):
-        """
-        TODO
-        """
+        """Group alignments by read ID base."""
+
         yield ID_base(ID), (ID, alignment)
 
     def reducer(self, ID_base, alignments):
         """
-        TODO
+        Make alignment pairs and filter. Emit valid alignments individually.
+        
+        Only Splat alignments are returned, CashX alignments are not.
+
+        Valid alignments must have...
+          - the same chromosome
+          - opposite strands
+          - a distance between them within the given bounds.
         """
+
+        valid = set()
         alignments = list(alignments)
 
-        for (x_ID, x), (y_ID, y) in combinations(alignments, 2):
+        for ix, iy in combinations(xrange(len(alignments)), 2):
+            x = alignments[ix][1]
+            y = alignments[iy][1]
 
-            if x != y and x['chromosome'] == y['chromosome'] and \
-               x['strand'] != y['strand']:
+            if x['chromosome'] == y['chromosome'] and x['strand'] != y['strand']:
 
                 d = distance_between(x, y)
                 if d >= self.options.min_distance and d <= self.options.max_distance:
 
                     if x['type'] == 'splat':
-                        yield x, x_ID
+                        valid.add(ix)
 
                     if y['type'] == 'splat':
-                        yield y, y_ID
+                        valid.add(iy)
 
-    def duplicates_reducer(self, splat, IDs):
-        """
-        TODO
-        """
-        for ID in set(IDs):
-            yield ID, splat
-
-    def steps(self):
-        """
-        TODO
-        """
-        return [self.mr(mapper=self.mapper, reducer=self.reducer),
-                self.mr(reducer=self.duplicates_reducer)]
+        for iv in valid:
+            yield alignments[iv][0], alignments[iv][1]
 
 
 if __name__ == '__main__':
