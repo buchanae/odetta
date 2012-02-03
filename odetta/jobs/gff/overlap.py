1b2281f43b6dd5c64537abbd5f63f16d311c615f
Alex Buchanan <buchanae@gmail.com>
2012-02-03 15:56:52 -0800

    ugh, i suck, fix import error caused by last commit

--- odetta/jobs/gff/overlap.py
+++ odetta/jobs/gff/overlap.py
@@ -5,7 +5,7 @@ from mrjob.protocol import RawValueProtocol
 #TODO maybe make rtree optional and fall back on pyrtree?
 import rtree
 
-from odetta.gff.feature import Feature
+from feature import Feature
 
 
 def calc_overlap(a, b):
